5cc5451e89ccfcb8b1663902b887c13fa1cc1a9d
amiravni16 <amiravni13@gmail.com>
2025-03-06 20:35:08 +0200

    Change onBallClick

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 function onBallClick() {
-    console.log("The ball was clicked!")
-  }
-  
-  
   const ball = document.querySelector(".ball")
-  ball.addEventListener("click", onBallClick)
+  let currentSize = parseInt(window.getComputedStyle(ball).width)
+  const newSize = currentSize + 50
+  ball.style.width = `${newSize}px`
+  ball.style.height = `${newSize}px`
+  ball.textContent = newSize
+}
+
+const ball = document.querySelector(".ball")
+ball.addEventListener("click", onBallClick)
