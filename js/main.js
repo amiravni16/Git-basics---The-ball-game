d2a8769b810991c415ce7fa3afb78ba1a32649e1
amiravni16 <amiravni13@gmail.com>
2025-03-06 23:31:58 +0200

    Add second ball

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-function onBallClick() {
-  
+'use strict'
+
+function onBallClick(event) {
+  var ball = event.target
   var currentSize = parseInt(window.getComputedStyle(ball).width)
-  var randomIncrement = getRandomInt(20, 60)
+  var randomIncrement = getRandomInt(20, 60) 
   var newSize = currentSize + randomIncrement
 
   if (newSize > 400) {
@@ -11,7 +13,12 @@ function onBallClick() {
   ball.style.width = `${newSize}px`
   ball.style.height = `${newSize}px`
   ball.textContent = newSize
-  ball.style.backgroundColor = getRandomColor()
+  ball.style.backgroundColor = getRandomColor() 
 }
-const ball = document.querySelector(".ball")
-ball.addEventListener("click", onBallClick)
+
+
+var ball1 = document.querySelector(".ball")
+var ball2 = document.querySelector(".ball2")
+
+ball1.addEventListener("click", onBallClick)
+ball2.addEventListener("click", onBallClick)
